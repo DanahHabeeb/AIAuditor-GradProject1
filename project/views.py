dc0b7d8cc83954692e7933995f6535ded9305f8d
اريج احمد صالح <2190006001@iau.edu.sa>
2024-03-13 01:45:34 +0300

    areej updates1

---
+++ project/views.py
@@ -0,0 +1,35 @@
+﻿from django.shortcuts import render
+
+# Create your views here.
+
+posts = [
+    {
+        'title': ' تقرير الالتزام الأول ',
+        'content': ' نتائج حالة التزام الجهة بالضوابط، وتقرير الالتزام المقترح',
+        'post_date':'15-2-2024', 
+    },
+    
+    {
+        'title': ' تقرير الالتزام الثاني ',
+        'content': ' نتائج حالة التزام الجهة بالضوابط، وتقرير الالتزام المقترح',
+        'post_date':'15-3-2024', 
+    },
+    
+    {
+        'title': ' تقرير الالتزام الثالث ',
+        'content': ' نتائج حالة التزام الجهة بالضوابط، وتقرير الالتزام المقترح',
+        'post_date':'15-4-2024', 
+    },
+    
+    ]
+
+def home(request):
+    context = {
+        'title': 'الصفحة الرئيسية',
+        'posts': posts,
+        }
+    return render (request, 'project/index.html', context)
+
+
+def about(request): 
+    return render(request, 'project/about.html', {'title': 'من نحن'})
